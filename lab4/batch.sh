0035125e93abb80284ecd98367d802af6cc79571
Michał <michaljanpatyk@gmail.com>
2020-11-10 08:36:46 +0100

    Change batch parameters

--- lab4/batch.sh
+++ lab4/batch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
+#SBATCH --constraint="intel"
 #SBATCH --time=00:30:00
 #SBATCH --mem-per-cpu=100MB
 #SBATCH --partition=plgrid
@@ -8,14 +9,17 @@
 
 module load plgrid/tools/python-intel/3.6.5
 
-for n in 100 1000 100000; do
+for n in 1000 10000 100000; do
   echo "$n"
     for ((i=1; i<=$1; i++)); do
     echo "$i"
       for ((j=1; j<=$2; j++)); do
-        mpirun -np "$i" ./pcam_parallel_stripes.py $3 "$n" "$i"
+        mpirun -np "$i" ./pcam_parallel_stripes.py "$3" "$n" "$i"
       done
     done
 done
 
+# $1 - max tasks
+# $2 - computation repetitions
+# $3 - iterations
 # sbatch batch.sh 12 3 100
